e3fd75fc15e2e027480d5ad97926d963174a349c
RVRITIK-VERMA <ritikverma431@gmai.com>
2025-01-29 23:42:14 +0530

    old file

--- index.js
+++ index.js
@@ -20,4 +20,6 @@ app.get('/',(req,res)=>{
     res.send('Welcome to Hotel Management API');
 })
 
-module.exports = app;
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
